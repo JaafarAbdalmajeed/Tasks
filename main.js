dd90768d0b279902bc64efd911f267d1576ae565
JaafarAlwahsh <jaafaralwahsh22@gmail.com>
2023-11-15 17:08:34 +0300

    it is done

---
+++ main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let tasks = document.querySelectorAll('.task')
+    tasks.forEach(task => {
+        task.addEventListener('click', () => {
+            let link =  task.getAttribute('data-link')
+            let linkWebSite = link
+            if(link){
+                window.location.href = linkWebSite
+            } else {
+                console.log('not found 404')
+            }
+        })
+    });
+})
